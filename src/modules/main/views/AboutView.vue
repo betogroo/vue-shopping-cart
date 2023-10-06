b6df768ad68eade5a044ac7660c039ceee78448c
betogroo <luizhumberto@gmail.com>
2023-10-06 12:56:13 -0300

    chore: default scaffold

---
+++ src/modules/main/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img
+        height="500
+        "
+        src="@/assets/logo.png"
+      />
+      <div class="text-h4 text-teal-darken-4">Vue Shopping Cart</div>
+      <div class="text-teal-darken-4">
+        Git Hub Project <v-icon>mdi-git</v-icon>
+      </div>
+      <div class="py-14" />
+    </v-responsive>
+  </v-container>
+</template>
---
+++ src/modules/main/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container class="d-flex justify-center fill-height">
+    <v-responsive
+      class="text-center"
+      max-width="250"
+    >
+      <h1>Shopping Cart</h1>
+    </v-responsive>
+  </v-container>
+</template>
