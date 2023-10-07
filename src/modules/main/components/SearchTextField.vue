7d7f1a1df49bff20b7460e04eae6ad9a99fa0eeb
Luiz Humberto <luizhumberto@gmail.com>
2023-10-07 10:29:23 -0300

    feat: add handleSubmit when call function on icon click and submit form

--- src/modules/main/components/SearchTextField.vue
+++ src/modules/main/components/SearchTextField.vue
@@ -1,12 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchTerm = ref('')
+const handleSubmit = () => {
+  console.log(searchTerm.value)
+}
+</script>
 
 <template>
-  <v-text-field
-    append-inner-icon="mdi-magnify"
-    class="mx-4"
-    density="compact"
-    hide-details
-    placeholder="Procure seu produto"
-    variant="solo"
-  />
+  <v-form @submit.prevent="handleSubmit">
+    <v-text-field
+      v-model="searchTerm"
+      append-inner-icon="mdi-magnify"
+      class="mx-4"
+      density="compact"
+      hide-details
+      placeholder="Procure seu produto"
+      variant="solo"
+      @click:append-inner="handleSubmit"
+    />
+  </v-form>
 </template>
