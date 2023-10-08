e73fd0aa03941c1c49481971961b69e072b2f046
Luiz Humberto <luizhumberto@gmail.com>
2023-10-08 12:37:05 -0300

    chore: use zod to type schema

--- src/modules/main/types/Product.ts
+++ src/modules/main/types/Product.ts
@@ -1,4 +1,8 @@
-export interface Product {
-  id: number
-  name: string
-}
+import { z } from 'zod'
+
+const ProductSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+})
+
+export type Product = z.infer<typeof ProductSchema>
