7e6ee6582aaba24d9cca63a23f5a36dbd4ae5b80
Luiz Humberto <luizhumberto@gmail.com>
2023-10-10 17:00:55 -0300

    feat: list items in cart

--- src/modules/main/components/CartDrawer.vue
+++ src/modules/main/components/CartDrawer.vue
@@ -2,10 +2,16 @@
 import { useDisplay } from 'vuetify/lib/framework.mjs'
 import { storeToRefs } from 'pinia'
 import { useCartStore } from '../store/useCartStore'
+import { useHelpers } from '@/shared/composables'
 
 const { mobile } = useDisplay()
 const store = useCartStore()
-const { visible } = storeToRefs(store)
+const { visible, cartItems } = storeToRefs(store)
+const { localCurrency } = useHelpers()
+
+const handleDelete = (index: number) => {
+  console.log('Vai deletar', index)
+}
 </script>
 
 <template>
@@ -14,5 +20,30 @@ const { visible } = storeToRefs(store)
     :location="mobile ? 'bottom' : 'right'"
     temporary
   >
+    <v-list lines="two">
+      <v-list-item
+        v-for="(item, i) in cartItems"
+        :key="i"
+      >
+        <template #append>
+          <v-icon @click="handleDelete(i)">
+            mdi-trash-can-outline</v-icon
+          ></template
+        >
+        <template #title>{{ item.title }}</template>
+        <template #subtitle>
+          <h1 class="font-weight-bold text-right">
+            {{ localCurrency(item.price) }}
+          </h1></template
+        >
+        <template #prepend>
+          <v-avatar
+            :image="item.thumbnail"
+            size="40"
+            variant="elevated"
+          ></v-avatar>
+        </template>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
